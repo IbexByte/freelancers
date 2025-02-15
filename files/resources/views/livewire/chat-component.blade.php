b2927b523cfbf1913684fec7f8defcc9537b89ad
ibexbyte <brahim.semmani81@gmail.com>
2025-02-15 03:06:07 +0100

    chat optimize

--- files/resources/views/livewire/chat-component.blade.php
+++ files/resources/views/livewire/chat-component.blade.php
@@ -113,7 +113,7 @@
                 <!-- الرسائل -->
                 <div class="flex-1 bg-[#eee]  
                         no-scrollbar w-full overflow-y-auto 
-                        pb-28 pt-28 px-0 space-y-3 scroll-smooth"
+                        pb-32 pt-28 px-0 space-y-3 scroll-smooth"
                     x-data="{ scrollToBottom() { this.$el.scrollTop = this.$el.scrollHeight } }" x-init="scrollToBottom()" x-on:messageReceived.window="scrollToBottom()">
 
                     @foreach ($messages as $message)
